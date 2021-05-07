7fc572670f1ffbe8d4335ac42aab70e7fc3dc807
Tatu Wikman <tatu.wikman@gmail.com>
2021-05-07 14:11:45 +0300

    revert domain mangling for live, add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+next version
+============
+
+v0.0.2 - 20210507
+=================
+* fix useragent header
+* comment out clubservice not found test, production responds differently from staging
+* add new test case for person service
+* added CHANGELOG.md file
+
+v0.0.1 - 20210429
+=================
+* initial release
+* auth works, few testcases
--- src/Client.php
+++ src/Client.php
@@ -12,21 +12,9 @@ use tswfi\Ebirdie\WsseAuthHeader;
 class Client extends \SoapClient
 {
     const USERAGENT = "PHP-SOAP eBirdieClient";
-    // TODO: these can be removed if / when ebirdie fixes their live wsdl file
-    const STAGINGDOMAIN = "stage.uhs.golf.fi";
-    const LIVEDOMAIN = "api.golf.fi";
 
     public function __construct(string $wsdl, string $username, string $password)
     {
-        // this is ugly, we check the wsdl url domain name to know if we are
-        // in live or staging environment
-        // TODO: this can be removed if / when ebirdie fixes their live wsdl file
-        // default to staging
-        $this->production = false;
-        if (strpos($wsdl, self::LIVEDOMAIN) == true) {
-            $this->production = true;
-        }
-
         // send useragent string
         $streamContextOptions['http']['user_agent'] = self::USERAGENT;
 
@@ -52,20 +40,5 @@ class Client extends \SoapClient
         $this->__setSoapHeaders([$wsse_header]);
 
         return $this;
-    }
-
-    /**
-     * The current live wsdl edpoints point to staging
-     * If we are calling the live wsdl mangle the endpoint
-     * to live
-     *
-     * TODO: this can be removed if / when eBirdie changes their live wsdl
-     */
-    public function __doRequest($request, $location, $action, $version, $one_way = NULL)
-    {
-        if($this->production) {
-            $location = str_replace(self::STAGINGDOMAIN, self::LIVEDOMAIN, $location);
-        }
-        return parent::__doRequest($request, $location, $action, $version, $one_way);
     }
 }
